c85db27ab512f1514a107c5c8900199e2ebf6892
3739507 <s3739507@students.fscj.edu>
2026-09-08 14:04:41 -0400

    Create PalmerPenguins.java to introduce penguin species
    This program introduces the Palmer Penguins species and displays their names along with the total count of species.

---
+++ PalmerPenguins.java
@@ -0,0 +1,30 @@
+// palmerPenguin.java
+// Donny Nelson
+// 09/08/2026
+// This program introduces Palmer Penguins
+
+
+
+public class PalmerPenguins {
+
+      
+        static final String SP_CHINSTRAP = "Chinstrap";
+        static final String SP_GENTOO = "Gentoo";
+        static final String SP_ADELIE = "Adelie";
+        
+        static final int TOTAL_SPECIES = 3;
+       
+        public static void main(String[] args) {
+ 
+        
+         System.out.println ("Introducing the Palmer Penguins: ");
+         System.out.println ("\t" + SP_CHINSTRAP + "!");
+         System.out.println ("\t" + SP_GENTOO + "!");
+         System.out.println ("and last but not least...");
+         System.out.println ("\t" + SP_ADELIE + "!");
+         System.out.println ("There are a total of " + TOTAL_SPECIES + " penguin species in this dataset.");
+         
+         
+      }
+      
+   }
